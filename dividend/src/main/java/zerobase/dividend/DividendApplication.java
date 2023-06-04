608976bb2e92c86c205d51db558c69905636b1cc
ming <bamormiog@naver.com>
2023-06-04 21:12:31 +0900

    Initial commit

---
+++ dividend/src/main/java/zerobase/dividend/DividendApplication.java
@@ -0,0 +1,13 @@
+package zerobase.dividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class DividendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DividendApplication.class, args);
+	}
+}
